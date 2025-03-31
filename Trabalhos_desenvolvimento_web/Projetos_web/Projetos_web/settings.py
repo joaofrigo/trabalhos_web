3d6f5cb6225c34a5be7d210d07f61a54b6e82079
joão marcos <joaofrigo1@gmail.com>
2025-03-31 15:46:58 -0300

    django sem debug deploy trabalho 1

--- Trabalhos_desenvolvimento_web/Projetos_web/Projetos_web/settings.py
+++ Trabalhos_desenvolvimento_web/Projetos_web/Projetos_web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xlprhk%@vme+f2a381y$t%7thfiadhexr&du$kg4e%jya+m+%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'brief-eudora-sou-aluno-46f86fb9.koyeb.app']
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
