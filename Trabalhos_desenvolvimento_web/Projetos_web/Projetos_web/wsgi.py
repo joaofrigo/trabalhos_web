1036d6b56119b82c7f188f116a9a424061cfccab
joão marcos <joaofrigo1@gmail.com>
2025-03-31 11:26:37 -0300

    teste deploy 8

--- Trabalhos_desenvolvimento_web/Projetos_web/Projetos_web/wsgi.py
+++ Trabalhos_desenvolvimento_web/Projetos_web/Projetos_web/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projetos_web.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', './settings.py')
 
 application = get_wsgi_application()
